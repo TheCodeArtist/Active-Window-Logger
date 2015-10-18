f61e3d8748622563a23db4512b935fef9d4fe1cd
ChinmayVS <cvs268@gmail.com>
2015-10-18 23:42:55 +0530

    MVP - Added first prototype
    First working prototype that allows to launch and run the app to log the
    active foreground window on the system.
    
    Contains simple features like :
    - Pause/Resume Active window logging.
    - Ignoring own application window.
    - Consolidating consecutive entries of same window.
    - Allow deleting any spurious entries from current log.
    - Exporting log entries as CSV for storage/analysis.
    
    also added /ReferenceMaterials directory with WIP notes and other
    miscellany.
    
    Built and tested using VB2008Express on win7-x64.

---
+++ ActiveWindowLogger/frmMain.Designer.vb
@@ -0,0 +1,304 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Me.tmrPoll = New System.Windows.Forms.Timer(Me.components)
+        Me.txtPID = New System.Windows.Forms.TextBox
+        Me.txtProcessName = New System.Windows.Forms.TextBox
+        Me.txtMainWindowTitle = New System.Windows.Forms.TextBox
+        Me.txtWindowTitle = New System.Windows.Forms.TextBox
+        Me.txtWindowStatus = New System.Windows.Forms.TextBox
+        Me.chkStatus = New System.Windows.Forms.CheckBox
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox
+        Me.lblStatus = New System.Windows.Forms.Label
+        Me.tmrMinute = New System.Windows.Forms.Timer(Me.components)
+        Me.btnExportSelected = New System.Windows.Forms.Button
+        Me.chkSelect = New System.Windows.Forms.CheckBox
+        Me.btnDeleteChecked = New System.Windows.Forms.Button
+        Me.lvEntries = New System.Windows.Forms.ListView
+        Me.ColumnHeader1 = New System.Windows.Forms.ColumnHeader
+        Me.ColumnHeader2 = New System.Windows.Forms.ColumnHeader
+        Me.ColumnHeader3 = New System.Windows.Forms.ColumnHeader
+        Me.ColumnHeader4 = New System.Windows.Forms.ColumnHeader
+        Me.ColumnHeader5 = New System.Windows.Forms.ColumnHeader
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'tmrPoll
+        '
+        Me.tmrPoll.Enabled = True
+        Me.tmrPoll.Interval = 200
+        '
+        'txtPID
+        '
+        Me.txtPID.BackColor = System.Drawing.SystemColors.Control
+        Me.txtPID.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtPID.Cursor = System.Windows.Forms.Cursors.Default
+        Me.txtPID.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPID.ForeColor = System.Drawing.Color.Black
+        Me.txtPID.Location = New System.Drawing.Point(12, 19)
+        Me.txtPID.MinimumSize = New System.Drawing.Size(400, 18)
+        Me.txtPID.Name = "txtPID"
+        Me.txtPID.ReadOnly = True
+        Me.txtPID.ShortcutsEnabled = False
+        Me.txtPID.Size = New System.Drawing.Size(400, 15)
+        Me.txtPID.TabIndex = 1
+        Me.txtPID.TabStop = False
+        Me.txtPID.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.txtPID.WordWrap = False
+        '
+        'txtProcessName
+        '
+        Me.txtProcessName.BackColor = System.Drawing.SystemColors.Control
+        Me.txtProcessName.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtProcessName.Cursor = System.Windows.Forms.Cursors.Default
+        Me.txtProcessName.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtProcessName.ForeColor = System.Drawing.Color.Black
+        Me.txtProcessName.Location = New System.Drawing.Point(12, 40)
+        Me.txtProcessName.MinimumSize = New System.Drawing.Size(400, 18)
+        Me.txtProcessName.Name = "txtProcessName"
+        Me.txtProcessName.ReadOnly = True
+        Me.txtProcessName.ShortcutsEnabled = False
+        Me.txtProcessName.Size = New System.Drawing.Size(400, 15)
+        Me.txtProcessName.TabIndex = 2
+        Me.txtProcessName.TabStop = False
+        Me.txtProcessName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.txtProcessName.WordWrap = False
+        '
+        'txtMainWindowTitle
+        '
+        Me.txtMainWindowTitle.BackColor = System.Drawing.SystemColors.Control
+        Me.txtMainWindowTitle.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtMainWindowTitle.Cursor = System.Windows.Forms.Cursors.Default
+        Me.txtMainWindowTitle.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtMainWindowTitle.ForeColor = System.Drawing.Color.Black
+        Me.txtMainWindowTitle.Location = New System.Drawing.Point(12, 61)
+        Me.txtMainWindowTitle.MinimumSize = New System.Drawing.Size(400, 18)
+        Me.txtMainWindowTitle.Name = "txtMainWindowTitle"
+        Me.txtMainWindowTitle.ReadOnly = True
+        Me.txtMainWindowTitle.ShortcutsEnabled = False
+        Me.txtMainWindowTitle.Size = New System.Drawing.Size(400, 15)
+        Me.txtMainWindowTitle.TabIndex = 3
+        Me.txtMainWindowTitle.TabStop = False
+        Me.txtMainWindowTitle.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.txtMainWindowTitle.WordWrap = False
+        '
+        'txtWindowTitle
+        '
+        Me.txtWindowTitle.BackColor = System.Drawing.SystemColors.Control
+        Me.txtWindowTitle.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtWindowTitle.Cursor = System.Windows.Forms.Cursors.Default
+        Me.txtWindowTitle.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtWindowTitle.ForeColor = System.Drawing.Color.Black
+        Me.txtWindowTitle.Location = New System.Drawing.Point(12, 82)
+        Me.txtWindowTitle.MinimumSize = New System.Drawing.Size(400, 18)
+        Me.txtWindowTitle.Name = "txtWindowTitle"
+        Me.txtWindowTitle.ReadOnly = True
+        Me.txtWindowTitle.ShortcutsEnabled = False
+        Me.txtWindowTitle.Size = New System.Drawing.Size(400, 15)
+        Me.txtWindowTitle.TabIndex = 4
+        Me.txtWindowTitle.TabStop = False
+        Me.txtWindowTitle.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.txtWindowTitle.WordWrap = False
+        '
+        'txtWindowStatus
+        '
+        Me.txtWindowStatus.BackColor = System.Drawing.SystemColors.Control
+        Me.txtWindowStatus.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtWindowStatus.Cursor = System.Windows.Forms.Cursors.Default
+        Me.txtWindowStatus.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtWindowStatus.Location = New System.Drawing.Point(12, 103)
+        Me.txtWindowStatus.MinimumSize = New System.Drawing.Size(400, 18)
+        Me.txtWindowStatus.Name = "txtWindowStatus"
+        Me.txtWindowStatus.ReadOnly = True
+        Me.txtWindowStatus.ShortcutsEnabled = False
+        Me.txtWindowStatus.Size = New System.Drawing.Size(400, 15)
+        Me.txtWindowStatus.TabIndex = 5
+        Me.txtWindowStatus.TabStop = False
+        Me.txtWindowStatus.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.txtWindowStatus.WordWrap = False
+        '
+        'chkStatus
+        '
+        Me.chkStatus.Appearance = System.Windows.Forms.Appearance.Button
+        Me.chkStatus.BackColor = System.Drawing.Color.IndianRed
+        Me.chkStatus.CheckAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.chkStatus.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.chkStatus.ForeColor = System.Drawing.Color.Snow
+        Me.chkStatus.Location = New System.Drawing.Point(12, 12)
+        Me.chkStatus.Name = "chkStatus"
+        Me.chkStatus.Size = New System.Drawing.Size(180, 124)
+        Me.chkStatus.TabIndex = 7
+        Me.chkStatus.TabStop = False
+        Me.chkStatus.Text = "" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "STOPPED" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.chkStatus.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.chkStatus.UseVisualStyleBackColor = False
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtPID)
+        Me.GroupBox1.Controls.Add(Me.txtProcessName)
+        Me.GroupBox1.Controls.Add(Me.txtMainWindowTitle)
+        Me.GroupBox1.Controls.Add(Me.txtWindowStatus)
+        Me.GroupBox1.Controls.Add(Me.txtWindowTitle)
+        Me.GroupBox1.Cursor = System.Windows.Forms.Cursors.Default
+        Me.GroupBox1.Location = New System.Drawing.Point(198, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(424, 125)
+        Me.GroupBox1.TabIndex = 8
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Current Window Details"
+        '
+        'lblStatus
+        '
+        Me.lblStatus.BackColor = System.Drawing.Color.LemonChiffon
+        Me.lblStatus.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblStatus.Location = New System.Drawing.Point(18, 101)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(168, 30)
+        Me.lblStatus.TabIndex = 0
+        Me.lblStatus.Text = "0 windows" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "since 0 minutes"
+        Me.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'tmrMinute
+        '
+        Me.tmrMinute.Enabled = True
+        Me.tmrMinute.Interval = 60000
+        '
+        'btnExportSelected
+        '
+        Me.btnExportSelected.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnExportSelected.Location = New System.Drawing.Point(482, 305)
+        Me.btnExportSelected.Name = "btnExportSelected"
+        Me.btnExportSelected.Size = New System.Drawing.Size(140, 20)
+        Me.btnExportSelected.TabIndex = 18
+        Me.btnExportSelected.Text = "Export Selected Entries"
+        Me.btnExportSelected.UseVisualStyleBackColor = True
+        '
+        'chkSelect
+        '
+        Me.chkSelect.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.chkSelect.Location = New System.Drawing.Point(12, 305)
+        Me.chkSelect.Name = "chkSelect"
+        Me.chkSelect.Size = New System.Drawing.Size(91, 18)
+        Me.chkSelect.TabIndex = 17
+        Me.chkSelect.Text = "Select Entries"
+        Me.chkSelect.UseVisualStyleBackColor = True
+        '
+        'btnDeleteChecked
+        '
+        Me.btnDeleteChecked.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnDeleteChecked.Location = New System.Drawing.Point(336, 305)
+        Me.btnDeleteChecked.Name = "btnDeleteChecked"
+        Me.btnDeleteChecked.Size = New System.Drawing.Size(140, 20)
+        Me.btnDeleteChecked.TabIndex = 16
+        Me.btnDeleteChecked.Text = "Delete Selected Entries"
+        Me.btnDeleteChecked.UseVisualStyleBackColor = True
+        '
+        'lvEntries
+        '
+        Me.lvEntries.CheckBoxes = True
+        Me.lvEntries.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3, Me.ColumnHeader4, Me.ColumnHeader5})
+        Me.lvEntries.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lvEntries.Location = New System.Drawing.Point(12, 144)
+        Me.lvEntries.Name = "lvEntries"
+        Me.lvEntries.Size = New System.Drawing.Size(610, 150)
+        Me.lvEntries.TabIndex = 15
+        Me.lvEntries.UseCompatibleStateImageBehavior = False
+        Me.lvEntries.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "PID"
+        Me.ColumnHeader1.Width = 70
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Process Name"
+        Me.ColumnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.ColumnHeader2.Width = 100
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "Main Window Title"
+        Me.ColumnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.ColumnHeader3.Width = 150
+        '
+        'ColumnHeader4
+        '
+        Me.ColumnHeader4.Text = "Window Title"
+        Me.ColumnHeader4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.ColumnHeader4.Width = 150
+        '
+        'ColumnHeader5
+        '
+        Me.ColumnHeader5.Text = "TimeStamp"
+        Me.ColumnHeader5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.ColumnHeader5.Width = 115
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(634, 335)
+        Me.Controls.Add(Me.btnExportSelected)
+        Me.Controls.Add(Me.chkSelect)
+        Me.Controls.Add(Me.btnDeleteChecked)
+        Me.Controls.Add(Me.lvEntries)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.lblStatus)
+        Me.Controls.Add(Me.chkStatus)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Name = "frmMain"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Active Window Logger"
+        Me.TopMost = True
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents tmrPoll As System.Windows.Forms.Timer
+    Friend WithEvents txtPID As System.Windows.Forms.TextBox
+    Friend WithEvents txtProcessName As System.Windows.Forms.TextBox
+    Friend WithEvents txtMainWindowTitle As System.Windows.Forms.TextBox
+    Friend WithEvents txtWindowTitle As System.Windows.Forms.TextBox
+    Friend WithEvents txtWindowStatus As System.Windows.Forms.TextBox
+    Friend WithEvents chkStatus As System.Windows.Forms.CheckBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents lblStatus As System.Windows.Forms.Label
+    Private WithEvents tmrMinute As System.Windows.Forms.Timer
+    Friend WithEvents btnExportSelected As System.Windows.Forms.Button
+    Friend WithEvents chkSelect As System.Windows.Forms.CheckBox
+    Friend WithEvents btnDeleteChecked As System.Windows.Forms.Button
+    Friend WithEvents lvEntries As System.Windows.Forms.ListView
+    Friend WithEvents ColumnHeader1 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader2 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader3 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader4 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader5 As System.Windows.Forms.ColumnHeader
+
+End Class
---
+++ ActiveWindowLogger/frmMain.vb
@@ -0,0 +1,164 @@
+﻿Option Explicit On
+Imports System.Data
+Imports System.Data.OleDb
+
+Public Class frmMain
+    Private Declare Function GetForegroundWindow Lib "user32.dll" () As IntPtr
+    Private Declare Function GetWindowThreadProcessId Lib "user32.dll" (ByVal hwnd As IntPtr, ByRef lpdwProcessID As Integer) As Integer
+    Private Declare Function GetWindowText Lib "user32.dll" Alias "GetWindowTextA" (ByVal hWnd As IntPtr, ByVal WinTitle As String, ByVal MaxLength As Integer) As Integer
+    Private Declare Function GetWindowTextLength Lib "user32.dll" Alias "GetWindowTextLengthA" (ByVal hwnd As Long) As Integer
+
+    Dim AppLaunchTime As New Date
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmrPoll.Tick
+
+        ' Get the Handle to the Current Foreground Window
+        Dim hWnd As IntPtr = GetForegroundWindow()
+        If hWnd = IntPtr.Zero Then Exit Sub
+
+        ' Find the Length of the Window's Title
+        Dim TitleLength As Integer
+        TitleLength = GetWindowTextLength(hWnd)
+
+        ' Find the Window's Title
+        Dim WindowTitle As String = StrDup(TitleLength + 1, "*")
+        GetWindowText(hWnd, WindowTitle, TitleLength + 1)
+
+        ' Find the PID of the Application that Owns the Window
+        Dim pid As Integer = 0
+        GetWindowThreadProcessId(hWnd, pid)
+        If pid = 0 Then Exit Sub
+
+        ' Get the actual PROCESS from the PID
+        Dim proc As Process = Process.GetProcessById(pid)
+        If proc Is Nothing Then Exit Sub
+
+        ' Populate the textboxes with current data
+        txtPID.Text = pid.ToString
+        txtProcessName.Text = proc.ProcessName
+        txtMainWindowTitle.Text = proc.MainWindowTitle
+        txtWindowTitle.Text = WindowTitle
+
+        ' Ignore own Window
+        If txtWindowTitle.Text.Equals(Me.Text) Then
+            txtWindowStatus.Text = "Ignoring " + Me.Text
+            Exit Sub
+        End If
+
+        ' If WindowsTitles of new entry and last entry match, skip/consolidate
+        If lvEntries.Items.Count Then
+            If txtWindowTitle.Text.Equals(lvEntries.Items(0).SubItems(3).Text) Then
+                txtWindowStatus.Text = "Same window already active"
+                Exit Sub
+            End If
+        End If
+
+        ' Populate the listview with new entry of above current data
+        Dim newEntry As ListViewItem
+        newEntry = lvEntries.Items.Insert(0, txtPID.Text)
+        newEntry.SubItems.Add(txtProcessName.Text)
+        newEntry.SubItems.Add(txtMainWindowTitle.Text)
+        newEntry.SubItems.Add(txtWindowTitle.Text)
+        newEntry.SubItems.Add(Format(Now, "yyyy/MM/dd HH:mm:ss"))
+        txtWindowStatus.Text = "Started tracking active window"
+
+        lblStatus.Text = lvEntries.Items.Count.ToString + " windows" + vbCrLf + "since " + DateDiff(DateInterval.Minute, AppLaunchTime, DateTime.UtcNow).ToString + " minutes"
+
+    End Sub
+
+    Private Sub chkStatus_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles chkStatus.CheckedChanged
+        If chkStatus.Checked Then
+            chkStatus.BackColor = Color.DarkGreen
+            chkStatus.Text = vbCrLf + "Logging Active Windows" + vbCrLf + vbCrLf + "( click to pause )"
+            tmrPoll.Enabled = True
+        Else
+            chkStatus.BackColor = Color.IndianRed
+            chkStatus.Text = vbCrLf + "Paused" + vbCrLf + vbCrLf + "( click to resume logging )"
+            tmrPoll.Enabled = False
+        End If
+    End Sub
+
+    Private Sub frmMain_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        chkStatus.Checked = True
+        AppLaunchTime = DateTime.UtcNow
+    End Sub
+
+    Private Sub tmrMinute_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmrMinute.Tick
+        lblStatus.Text = lvEntries.Items.Count.ToString + " windows" + vbCrLf + "since " + DateDiff(DateInterval.Minute, AppLaunchTime, DateTime.UtcNow).ToString + " minutes"
+    End Sub
+
+    Private Sub lvEntries_ItemChecked(ByVal sender As Object, ByVal e As System.Windows.Forms.ItemCheckedEventArgs) Handles lvEntries.ItemChecked
+        If (lvEntries.Items.Count <> lvEntries.CheckedItems.Count) And (lvEntries.CheckedItems.Count > 0) Then
+            chkSelect.CheckState = CheckState.Indeterminate
+        ElseIf lvEntries.Items.Count = lvEntries.CheckedItems.Count Then
+            chkSelect.CheckState = CheckState.Checked
+        ElseIf lvEntries.CheckedItems.Count = 0 Then
+            chkSelect.CheckState = CheckState.Unchecked
+        End If
+    End Sub
+
+    Private Sub btnDeleteChecked_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDeleteChecked.Click
+        For i = (lvEntries.Items.Count - 1) To 0 Step -1
+            If lvEntries.Items(i).Checked = True Then
+                lvEntries.Items.RemoveAt(i)
+            End If
+        Next
+    End Sub
+
+    Private Sub chkSelect_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles chkSelect.Click
+        If chkSelect.CheckState = CheckState.Checked Then
+            Dim item As ListViewItem
+            For Each item In lvEntries.Items
+                item.Checked = True
+            Next
+        ElseIf chkSelect.CheckState = CheckState.Unchecked Then
+            Dim item As ListViewItem
+            For Each item In lvEntries.CheckedItems
+                item.Checked = False
+            Next
+        End If
+    End Sub
+
+    Private Sub btnExportSelected_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnExportSelected.Click
+
+        ' Prompt to select location to export
+        Dim sfd As New SaveFileDialog With { _
+        .Title = "Choose file to save to", _
+        .FileName = Format(Now, "dd-MMM-yyyy-hh-mm-tt") + "-activity.csv", _
+        .Filter = "CSV (*.csv)|*.csv", _
+        .FilterIndex = 0, _
+        .InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory)}
+
+        'show the dialog + display the results in a msgbox unless cancelled
+
+        If sfd.ShowDialog = DialogResult.OK Then
+
+            Dim headers = (From ch In lvEntries.Columns _
+                     Let header = DirectCast(ch, ColumnHeader) _
+                     Select header.Text).ToArray()
+
+            Dim items() = (From item In lvEntries.CheckedItems _
+                  Let lvi = DirectCast(item, ListViewItem) _
+                  Select (From subitem In lvi.SubItems _
+                      Let si = DirectCast(subitem, ListViewItem.ListViewSubItem) _
+                      Select si.Text).ToArray()).ToArray()
+
+            Dim table As String = String.Join(",", headers) & Environment.NewLine
+            For Each a In items
+                table &= String.Join(",", a) & Environment.NewLine
+            Next
+
+            table = table.TrimEnd(CChar(vbCr), CChar(vbLf))
+
+            ' Add retry/abort here in case file write fails (eg. if file open in other app)
+            IO.File.WriteAllText(sfd.FileName, table)
+
+        End If
+
+    End Sub
+
+    Private Sub frmMain_Resize(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Resize
+        lvEntries.Width = Me.Width - 30
+        lvEntries.Height = Me.Height - 216
+    End Sub
+End Class
